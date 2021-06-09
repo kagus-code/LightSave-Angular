c88d53419b5a507891faf2ab8509276fc5b3677e
Eston Kagwima <ekagwima745@gmail.com>
2021-06-09 10:28:25 +0300

    adding navbar html content and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingComponent} from './landing/landing.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component'
+const routes: Routes = [
+  { path:'',component: LandingComponent},
+  { path:'login',component: LoginComponent},
+  { path:'register',component: RegisterComponent},
 
-const routes: Routes = [];
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
