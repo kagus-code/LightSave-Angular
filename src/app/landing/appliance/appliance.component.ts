0a774fe963b28e86cafecdb8ef155352c1714978
Eston Kagwima <ekagwima745@gmail.com>
2021-06-19 01:00:21 +0300

    fix typo for month

--- src/app/landing/appliance/appliance.component.ts
+++ src/app/landing/appliance/appliance.component.ts
@@ -102,7 +102,7 @@ this.kwhYear =kwhYear;
 console.log(kwhDay+"--"+kwhMonth+"--"+kwhYear);
 
 this.resDay ="Energy Consumed per Day &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhDay + "&nbsp; (units kw/h)  "
-this.resMonth= "Energy Consumed Mohth &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhMonth + "&nbsp; (units kw/h) " 
+this.resMonth= "Energy Consumed Month &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhMonth + "&nbsp; (units kw/h) " 
 this.resYear ="Energy Consumed Year &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhYear + "&nbsp; (units kw/h)"
 this.applianceForm.reset();
 event.target.dayHrs.value = " "
@@ -149,7 +149,7 @@ this.kwhYearCost = Math.round(kwhYearCost)
 
 console.log(this.kwhDayCost+"--"+this.kwhMonthCost+"--"+this.kwhYearCost);
 this.resDay ="Cost per Day &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhDayCost + "&nbsp; shillings "
-this.resMonth= "Cost per Mohth &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhMonthCost + "&nbsp; shillings " 
+this.resMonth= "Cost per Month &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhMonthCost + "&nbsp; shillings " 
 this.resYear ="Cost per Year &nbsp;&nbsp;---&nbsp;&nbsp;" +this.kwhYearCost + "&nbsp; shillings "
 this.costForm.reset();
 event.target.dayHrs.value = " "
